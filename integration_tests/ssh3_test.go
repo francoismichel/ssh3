ec9f59574e08a81999c9e5bc0adbead04f494a02
François Michel <francois.michel@uclouvain.be>
2023-12-12 15:54:48 +0000

    add integration tests checking status code and ensuring users cannot log in using an uninstalled key

--- integration_tests/ssh3_test.go
+++ integration_tests/ssh3_test.go
@@ -16,10 +16,16 @@ var ssh3ServerPath string
 const DEFAULT_URL_PATH = "/ssh3-tests"
 var serverCommand *exec.Cmd
 var serverSession *Session
+var privKeyPath string
+var attackerPrivKeyPath string
+var username string
 // must exist on the machine to successfully run the tests
-const TESTUSER = "ssh3-testuser"
-const TESTPASSWD = "ssh3-testpasswd"
 const serverBind = "127.0.0.1:4433"
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
 
 var _ = BeforeSuite(func() {
 	var err error
@@ -42,6 +48,12 @@ var _ = BeforeSuite(func() {
 		serverCommand.Env = append(serverCommand.Env, "SSH3_LOG_LEVEL=debug")
 		serverSession, err = Start(serverCommand, GinkgoWriter, GinkgoWriter)
 		Expect(err).ToNot(HaveOccurred())
+
+		privKeyPath = os.Getenv("TESTUSER_PRIVKEY")
+		attackerPrivKeyPath = os.Getenv("ATTACKER_PRIVKEY")
+		username = os.Getenv("TESTUSER_USERNAME")
+		Expect(fileExists(privKeyPath)).To(BeTrue())
+		Expect(fileExists(attackerPrivKeyPath)).To(BeTrue())
 	}
 })
 
@@ -65,25 +77,72 @@ var _ = Describe("Testing the ssh3 cli", func() {
 	})
 
 	Context("With running server", func() {
-		var privKeyPath = os.Getenv("TESTUSER_PRIVKEY")
-		var username = os.Getenv("TESTUSER_USERNAME")
 		BeforeEach(func() {
 			if os.Getenv("SSH3_INTEGRATION_TESTS_WITH_SERVER_ENABLED") != "1" {
 				Skip("skipping integration tests")
+			} else {
 			}
 		})
 
 		Context("Insecure", func() {
-			It("Should connect using privkey", func() {
-				command := exec.Command(ssh3Path,
-										"-insecure",
-										"-privkey", privKeyPath,
-										fmt.Sprintf("%s@%s%s", username, serverBind, DEFAULT_URL_PATH),
-										"echo", "Hello, World!")
-				session, err := Start(command, GinkgoWriter, GinkgoWriter)
-				Expect(err).ToNot(HaveOccurred())
-				Eventually(session).Should(Exit(0))
-				Eventually(session).Should(Say("Hello, World!\n"))
+			var clientArgs []string
+			getClientArgs := func(privKeyPath string) []string {
+				return []string{
+					"-insecure",
+					"-privkey", privKeyPath,
+					fmt.Sprintf("%s@%s%s", username, serverBind, DEFAULT_URL_PATH),
+				}
+			}
+
+			Context("Client behaviour", func() {
+				It("Should connect using privkey", func() {
+					clientArgs = append(getClientArgs(privKeyPath), "echo", "Hello, World!")
+					command := exec.Command(ssh3Path, clientArgs...)
+					session, err := Start(command, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(session).Should(Exit(0))
+					Eventually(session).Should(Say("Hello, World!\n"))
+				})
+	
+				It("Should return the correct exit status", func() {
+					clientArgs0 := append(getClientArgs(privKeyPath), "exit", "0")
+					clientArgs1 := append(getClientArgs(privKeyPath), "exit", "1")
+					clientArgs255 := append(getClientArgs(privKeyPath), "exit", "255")
+					clientArgsMinus1 := append(getClientArgs(privKeyPath), "exit", "-1")
+	
+					command0 := exec.Command(ssh3Path, clientArgs0...)
+					session, err := Start(command0, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(session).Should(Exit(0))
+	
+					command1 := exec.Command(ssh3Path, clientArgs1...)
+					session, err = Start(command1, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(session).Should(Exit(1))
+	
+					command255 := exec.Command(ssh3Path, clientArgs255...)
+					session, err = Start(command255, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(session).Should(Exit(255))
+	
+					commandMinus1 := exec.Command(ssh3Path, clientArgsMinus1...)
+					session, err = Start(commandMinus1, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(session).Should(Exit(255))
+				})
+			})
+
+			Context("Server behaviour", func() {
+				It("Should not grand access to non-authorized identity", func() {
+					clientArgs = append(getClientArgs(attackerPrivKeyPath), "echo", "Hello, World!")
+
+					command := exec.Command(ssh3Path, clientArgs...)
+					session, err := Start(command, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(session).Should(Exit())
+					Eventually(session).ShouldNot(Exit(0))
+					Eventually(string(session.Wait().Err.Contents())).Should(ContainSubstring("unauthorized"))
+				})
 			})
 		})
 	})
