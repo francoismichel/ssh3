5798137cbb098e3d3f7b2e6caffcecd90162fcad
Theo Technicguy <19630890+TheoTechnicguy@users.noreply.github.com>
2023-12-18 22:42:32 +0100

    refactor(client): corrected review changes
    See <https://github.com/francoismichel/ssh3/pull/45>.

--- cli/client/main.go
+++ cli/client/main.go
@@ -413,32 +413,25 @@ func mainWithStatusCode() int {
 	}
 
 	hostnameIsAnIP := net.ParseIP(hostname) != nil
-	// *** Port selection ***
-	// There are six scenarios:
-	//
-	// | CLI port | parse | config port | program behaviour    |
-	// | -------- | ----- | ----------- | -------------------- |
-	// |    yes   |  nok  |     yes     | throw error and exit |
-	// |    yes   |  nok  |     no      | throw error and exit |
-	// |    yes   |  ok   |     yes     | use CLI port         |
-	// |    yes   |  ok   |     no      | use CLI port         |
-	// |    no    |  ---  |     yes     | use config port      |
-	// |    no    |  ---  |     no      | use default port     |
 
 	var port int
 	if urlPort != "" {
-		if parsedPort, err := strconv.Atoi(urlPort); err != nil || parsedPort > 0xffff {
+		if parsedPort, err := strconv.Atoi(urlPort); err == nil && parsedPort < 0xffff {
+			// There is a port in the CLI and the port is valid. Use the CLI port.
+			port = parsedPort
+		} else {
+			// There is a port in the CLI but it is not valid.
 			// use WithLevel(zerolog.FatalLevel) to log a fatal level, but let us handle
 			// program termination. log.Fatal() exits with os.Exit(1).
 			log.WithLevel(zerolog.FatalLevel).Str("Port", urlPort).Err(err).Msg("cli contains an invalid port")
 			fmt.Fprintf(os.Stderr, "Bad port '%s'\n", urlPort)
 			return -1
-		} else {
-			port = parsedPort
 		}
 	} else if configPort != -1 {
+		// There is no port in the CLI, but one in a config file. Use the config port.
 		port = configPort
 	} else {
+		// There is no port specified, neither in the CLI, nor in the configuration.
 		port = 443
 	}
 
