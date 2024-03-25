715e09231683319c2b5b916dbad39ed6bf4323fa
François Michel <francois.michel@uclouvain.be>
2024-03-25 12:16:29 +0000

    oops - add server_auth dir

---
+++ server_auth/auth.go
@@ -0,0 +1,97 @@
+package server_auth
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"runtime"
+	"strings"
+
+	"github.com/francoismichel/ssh3"
+	"github.com/francoismichel/ssh3/util/unix_util"
+
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/http3"
+	"github.com/rs/zerolog/log"
+)
+
+func HandleAuths(ctx context.Context, enablePasswordLogin bool, defaultMaxPacketSize uint64, handlerFunc ssh3.AuthenticatedHandlerFunc) (http.HandlerFunc, error) {
+	if runtime.GOOS != "linux" && enablePasswordLogin {
+		return nil, fmt.Errorf("password login not supported on %s/%s systems", runtime.GOOS, runtime.GOARCH)
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Server", ssh3.GetCurrentVersionString())
+		peerVersion, err := ssh3.ParseVersionString(r.UserAgent())
+		log.Debug().Msgf("received request from User-Agent %s", r.UserAgent())
+		log.Debug().Msgf("peer version: protocol version %s, software version %s", peerVersion.GetProtocolVersion(), peerVersion.GetSoftwareVersion())
+		// currently apply strict version rules
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Unsupported user-agent: %s", r.UserAgent()[:100]), http.StatusForbidden)
+			return
+		}
+		if !ssh3.IsVersionSupported(peerVersion) {
+			http.Error(w, fmt.Sprintf("Unsupported version: %s not supported by server with version %s", peerVersion.GetProtocolVersion(), ssh3.ThisVersion().GetProtocolVersion()), http.StatusForbidden)
+			return
+		}
+		// Only call Flush() here, as calling flush prevents from adding the Content-Length header to the response
+		// The Content-Length can be useful upon receiving an error response
+		defer w.(http.Flusher).Flush()
+		hijacker, ok := w.(http3.Hijacker)
+		if !ok { // should never happen, unless quic-go change their API
+			log.Error().Msgf("failed to hijack")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		streamCreator := hijacker.StreamCreator()
+		qconn := streamCreator.(quic.Connection)
+		if !qconn.ConnectionState().TLS.HandshakeComplete {
+			// do not process early data (0-RTT) when performing authorization
+			// to avoid replay attacks
+			w.WriteHeader(http.StatusTooEarly)
+			return
+		}
+		str := r.Body.(http3.HTTPStreamer).HTTPStream()
+		conv, err := ssh3.NewServerConversation(ctx, str, qconn, qconn, defaultMaxPacketSize, peerVersion)
+		if err != nil {
+			log.Error().Msgf("could not create new server conversation")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		convID := conv.ConversationID()
+		base64ConvID := base64.StdEncoding.EncodeToString(convID[:])
+		authorization := r.Header.Get("Authorization")
+		if enablePasswordLogin && strings.HasPrefix(authorization, "Basic ") {
+			HandleBasicAuth(handlerFunc, conv)(w, r)
+		} else if strings.HasPrefix(authorization, "Bearer ") {
+			username := r.URL.User.Username()
+			if username == "" {
+				username = r.URL.Query().Get("user")
+			}
+			HandleBearerAuth(username, base64ConvID, HandleJWTAuth(username, conv, handlerFunc))(w, r)
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+		}
+	}, nil
+}
+
+func HandleBasicAuth(handlerFunc ssh3.AuthenticatedHandlerFunc, conv *ssh3.Conversation) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		ok, err := unix_util.UserPasswordAuthentication(username, password)
+		if err != nil || !ok {
+			if err != nil {
+				log.Error().Msgf("user authentication failed: %s", err)
+			}
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		handlerFunc(username, conv, w, r)
+	}
+}
---
+++ server_auth/handlers.go
@@ -0,0 +1,89 @@
+package server_auth
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/francoismichel/ssh3"
+	"github.com/francoismichel/ssh3/util"
+	"github.com/francoismichel/ssh3/util/unix_util"
+
+	"github.com/rs/zerolog/log"
+)
+
+// BearerAuth returns the bearer token
+// Authorization header, if the request uses HTTP Basic Authentication.
+// See RFC 2617, Section 2.
+func BearerAuth(r *http.Request) (bearer string, ok bool) {
+	auth := r.Header.Get("Authorization")
+	if auth == "" {
+		return "", false
+	}
+	return parseBearerAuth(auth)
+}
+
+// parseBearerAuth parses an HTTP Bearer Authentication string.
+func parseBearerAuth(auth string) (bearer string, ok bool) {
+	const prefix = "Bearer "
+	// Case insensitive prefix match. See Issue 22736.
+	if len(auth) < len(prefix) || !util.EqualFold(auth[:len(prefix)], prefix) {
+		return "", false
+	}
+	// TODO: maybe validate the encoding format of the JWT token (at least verify that
+	// it is base64-encoded)
+	return string(auth[len(prefix):]), true
+}
+
+func HandleBearerAuth(username string, base64ConversationID string, handlerFunc ssh3.UnauthenticatedBearerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bearerString, ok := BearerAuth(r)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		handlerFunc(bearerString, base64ConversationID, w, r)
+	}
+}
+
+// currently only supports RS256 and EdDSA signing algorithms
+func HandleJWTAuth(username string, newConv *ssh3.Conversation, handlerFunc ssh3.AuthenticatedHandlerFunc) ssh3.UnauthenticatedBearerFunc {
+	return func(unauthenticatedBearerString string, base64ConversationID string, w http.ResponseWriter, r *http.Request) {
+		user, err := unix_util.GetUser(username)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		filenames := DefaultIdentitiesFileNames(user)
+		var identities []Identity
+		for _, filename := range filenames {
+			identitiesFile, err := os.Open(filename)
+			if err == nil {
+				newIdentities, err := ParseAuthorizedIdentitiesFile(user, identitiesFile)
+				if err != nil {
+					// TODO: logging
+					log.Error().Msgf("error when parsing authorized identities: %s", err)
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+				identities = append(identities, newIdentities...)
+			} else if !os.IsNotExist(err) {
+				log.Error().Msgf("error could not open %s: %s", filename, err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+		}
+
+		for _, identity := range identities {
+			verified := identity.Verify(util.JWTTokenString{Token: unauthenticatedBearerString}, base64ConversationID)
+			if verified {
+				// authentication successful
+				handlerFunc(username, newConv, w, r)
+				return
+			}
+		}
+
+		// TODO: logging
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+}
