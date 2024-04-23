e21ec78963c6c86f65497ff5d3ab908760999b18
François Michel <francois.michel@uclouvain.be>
2024-04-23 13:11:25 +0000

    add doc comments in plugins' plugins.go

--- auth/plugins/plugins.go
+++ auth/plugins/plugins.go
@@ -5,11 +5,16 @@ import (
 	"github.com/francoismichel/ssh3/internal"
 )
 
+// Each new plugin package must define an init() method (see https://go.dev/doc/effective_go#init)
+// in which they register one or more authentication plugins by calling either RegisterServerAuthPlugin()
+// for server-side auth plugins or RegisterClientAuthPlugin() for client-side auth plugins.
+
 // Registers a new server-side auth plugin
 func RegisterServerAuthPlugin(name string, plugin auth.ServerAuthPlugin) error {
 	return internal.RegisterServerAuthPlugin(name, plugin)
 }
 
+// Registers a new client-side auth plugin
 func RegisterClientAuthPlugin(name string, plugin auth.ClientAuthPlugin) error {
 	return internal.RegisterClientAuthPlugin(name, plugin)
 }
